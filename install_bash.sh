33e6d14d7e362dff239c48502360e3a94cb665a6
monirome <74182452+monirome@users.noreply.github.com>
2022-07-19 11:45:37 +0200

    Update install_bash.sh

--- install_bash.sh
+++ install_bash.sh
@@ -1,16 +1,24 @@
-##--------------------Install kenlm---------------------------------
+##--------------------Install python libraries-------------------------------------
+## pip install editdistance NO SE HA PODIDO INSTALAR 
+pip install sentencepiece
+pip install tensorboardX
+pip install tqdm
+pip install jiwer
+pip install hydra-ax-sweeper==1.2
+
+##--------------------Install kenlm-------------------------------------------------
 git clone --quiet https://github.com/kpu/kenlm && cd kenlm && mkdir -p build && cd build && cmake .. && make -j 4
 pip install https://github.com/kpu/kenlm/archive/master.zip
 
-##--------------------Install OpenBLAS---------------------------------
+##--------------------Install OpenBLAS---------------------------------------------
 git clone --quiet https://github.com/xianyi/OpenBLAS.git && cd OpenBLAS && mkdir -p build && cd build && \
     cmake .. && make -j 4 && make install
 
-##--------------------Install fftw---------------------------------
+##--------------------Install fftw-------------------------------------------------
 wget http://www.fftw.org/fftw-3.3.10.tar.gz && tar -xvf fftw-3.3.10.tar.gz && cd fftw-3.3.10 && \
     mkdir build && cd build && cmake .. && make -j 4 && make install -q
 
-##--------------------Install Faairseq---------------------------------
+##--------------------Install Faairseq---------------------------------------------
 git clone https://github.com/pytorch/fairseq &&
 cp docker/setup.py fairseq/. && \
 cp docker/audio_pretraining.py fairseq/fairseq/tasks/. && \
