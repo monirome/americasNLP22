c2a4c46a462dcea250f029900a84e4b3201cf456
monirome <74182452+monirome@users.noreply.github.com>
2022-07-19 13:42:18 +0200

    Update install_bash.sh

--- install_bash.sh
+++ install_bash.sh
@@ -1,3 +1,6 @@
+##----------------------Load apps -------------------------------------
+module load Python FFmpeg libsndfile wget zlib1g liblzma-dev
+##Libsox-fmt-mp3 Libboost-system-dev Libboost-thread-dev Libboost-program-options-dev Libboost-test-dev Libeigen3-dev sox  NO ESTAN EN CESVIMA
 ##--------------------Install python libraries-------------------------------------
 ## pip install editdistance NO SE HA PODIDO INSTALAR 
 pip install sentencepiece
