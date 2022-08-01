96f92fc023e5c457185a3a5d263ea4bc27f1359c
monirome <74182452+monirome@users.noreply.github.com>
2022-08-01 14:31:50 +0200

    Update job.sh

--- job.sh
+++ job.sh
@@ -3,11 +3,11 @@
 #SBATCH --partition=standard
 #SBATCH --job-name=prueba_job_monica.job
 #SBATCH --ntasks=1
-#SBATCH --mem-per-cpu=1G
-#SBATCH --time=22:00:00
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=romeroferronmonica@gmail.com
-#SBATCH --chdir=
+#SBATCH --mem-per-cpu=50gb
+##SBATCH --mail-type=ALL
+##SBATCH --mail-user=
+##SBATCH --chdir=
 ##------------------------ End job description ------------------------
+module load Python FFmpeg libsndfile 
 
-srun python fairseq/fairseq_cli/hydra_train.py
+srun fairseq-hydra-train --config-dir config --config-name finetune_quechua.yaml
