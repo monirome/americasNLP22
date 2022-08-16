f8a77d878653b88a36ed6bb9a76500fa218eace2
monirome <74182452+monirome@users.noreply.github.com>
2022-08-16 20:15:17 +0200

    Update install_bash.sh

--- install_bash.sh
+++ install_bash.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 ##----------------------Load apps -------------------------------------
 module load Python FFmpeg libsndfile wge libsndfile
-##zlib1g liblzma-dev Libsox-fmt-mp3 Libboost-system-dev Libboost-thread-dev Libboost-program-options-dev Libboost-test-dev Libeigen3-dev sox  NO ESTAN EN CESVIMA
+##zlib1g liblzma-dev Libsox-fmt-mp3 Libboost-system-dev Libboost-thread-dev Libboost-program-options$
 ##--------------------Install python libraries-------------------------------------
 pip install libsndfile1-dev
 pip install libsndfile1
 pip install SoundFile
+#pip install editdistance==0.1
 pip install editdistance 
 pip install sentencepiece
 pip install tensorboardX
@@ -13,10 +14,10 @@ pip install tqdm
 pip install jiwer
 pip install hydra-ax-sweeper==1.2
 ##--------------------Install kenlm-------------------------------------------------
-git clone --quiet https://github.com/kpu/kenlm && cd kenlm && mkdir -p build && cd build && cmake .. && make -j 4
+git clone --quiet https://github.com/kpu/kenlm && cd kenlm && mkdir -p build && cd build && cmake ..$
 pip install https://github.com/kpu/kenlm/archive/master.zip
 ##--------------------Install OpenBLAS---------------------------------------------
-git clone --quiet https://github.com/xianyi/OpenBLAS.git && cd OpenBLAS && mkdir -p build && cd build && \
+git clone --quiet https://github.com/xianyi/OpenBLAS.git && cd OpenBLAS && mkdir -p build && cd buil$
     cmake .. && make -j 4 && make install
 ##--------------------Install fftw-------------------------------------------------
 wget http://www.fftw.org/fftw-3.3.10.tar.gz && tar -xvf fftw-3.3.10.tar.gz && cd fftw-3.3.10 && \
