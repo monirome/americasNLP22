6f28b012650ea0f086ad6181c54bf28107a0d1fe
Monica Romero <74182452+monirome@users.noreply.github.com>
2022-08-18 08:07:39 +0200

    Add files via upload

---
+++ job_quechua.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+##----------------------- Start job description -----------------------
+#SBATCH --partition=standard
+#SBATCH --job-name=prueba_job_monica.job
+#SBATCH --ntasks=1
+#SBATCH --mem-per-cpu=50gb
+#SBATCH --partition=standard-gpu
+#SBATCH --gres=gpu:v100
+##SBATCH --mail-type=ALL
+#SBATCH --mail-user=romeroferronmonica@gmail.com
+##SBATCH --chdir=
+##------------------------ End job description ------------------------
+module load FFmpeg libsndfile Python 
+module load binutils
+
+srun fairseq-hydra-train --config-dir config --config-name finetune_quechua.yaml 
