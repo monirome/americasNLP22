85d97f9b8d69ce9c9be3ce52f327cba081bc5675
ivan <ivangonzaleztorre@gmail.com>
2022-08-19 21:13:51 +0200

    Fix

--- install_bash.sh
+++ install_bash.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+## ---------------------Known errors ---------------------------------
+# The following message is shown at the beginning of the installation but it does not affect:
+# "Lmod has detected the following error: These module(s) ... -11.2.0 GCCcore/10.2.0 to see how to load the module(s)."
+
 ##----------------------Load apps -------------------------------------
-module load Python FFmpeg libsndfile wget binutils
+module load Python FFmpeg libsndfile wget binutils CMake
 ##zlib1g liblzma-dev Libsox-fmt-mp3 Libboost-system-dev Libboost-thread-dev Libboost-program-options$
 
 ##--------------------Install python libraries-------------------------------------
@@ -17,12 +21,12 @@ pip install hydra-ax-sweeper==1.2
 
 ##--------------------Install kenlm-------------------------------------------------
 cd ~
-git clone --quiet https://github.com/kpu/kenlm && cd kenlm && mkdir -p build && cd build && cmake .. && make -j 4
+git clone https://github.com/kpu/kenlm && cd kenlm && mkdir -p build && cd build && cmake .. && make -j 4
 pip install https://github.com/kpu/kenlm/archive/master.zip
 
 ##--------------------Install OpenBLAS---------------------------------------------
 cd ~
-git clone --quiet https://github.com/xianyi/OpenBLAS.git && cd OpenBLAS && mkdir -p build && cd build && cmake .. && make -j 4 && make install
+git clone quiet https://github.com/xianyi/OpenBLAS.git && cd OpenBLAS && mkdir -p build && cd build && cmake .. && make -j 4 && make install
 
 ##--------------------Install fftw-------------------------------------------------
 cd ~
