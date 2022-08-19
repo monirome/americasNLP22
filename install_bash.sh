35e824aa8f995af243ef17818483bec29b7d5d54
ivan <ivangonzaleztorre@gmail.com>
2022-08-19 21:30:31 +0200

    Clean and update fixes

--- install_bash.sh
+++ install_bash.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
-## ---------------------Known errors ---------------------------------
-# None
-
 ##----------------------Load apps -------------------------------------
 module load Python FFmpeg libsndfile wget binutils CMake OpenBLAS FFTW Eigen Boost
-##zlib1g liblzma-dev Libsox-fmt-mp3 Libboost-system-dev Libboost-thread-dev Libboost-program-options$
+##zlib1g liblzma-dev Libsox-fmt-mp3 Libboost-system-dev Libboost-thread-dev Libboost-program-options libsndfile1-dev
 
 ##--------------------Install python libraries-------------------------------------
-#pip install libsndfile1-dev
-#pip install libsndfile1
 pip install SoundFile
-#pip install editdistance==0.1
-pip install editdistance 
+pip install editdistance
 pip install sentencepiece
 pip install tensorboardX
 pip install tqdm
@@ -19,22 +13,8 @@ pip install jiwer
 pip install hydra-ax-sweeper==1.2
 
 ##--------------------Install kenlm-------------------------------------------------
-git clone https://github.com/kpu/kenlm && cd kenlm && mkdir -p build && cd build && cmake .. && make -j 4
-cd ..
-cd ..
+git clone https://github.com/kpu/kenlm && cd kenlm && mkdir -p build && cd build && cmake .. && make -j 4 && cd ../..
 pip install https://github.com/kpu/kenlm/archive/master.zip
-
-##--------------------Install OpenBLAS---------------------------------------------
-## LOADED
-#cd ~
-#git clone https://github.com/xianyi/OpenBLAS.git && cd OpenBLAS && mkdir -p build && cd build && cmake .. && make -j 4 && make install
-
-##--------------------Install fftw-------------------------------------------------
-# LOADED
-#cd ~
-#wget http://www.fftw.org/fftw-3.3.10.tar.gz && tar -xvf fftw-3.3.10.tar.gz && cd fftw-3.3.10 && \
-#    mkdir build && cd build && cmake .. && make -j 4 && make install -q
-
 
 ##--------------------Install Fairseq--------------------------------------------
 git clone https://github.com/pytorch/fairseq && \
