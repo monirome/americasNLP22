b8edf23adddd64323fbcd3ed7560e3c0d4de70eb
Ivan Gonzalez Torres <u229765@login1.hpc.lan>
2022-08-19 18:50:38 +0000

    fixed installation

--- install_bash.sh
+++ install_bash.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 ##----------------------Load apps -------------------------------------
-module load Python FFmpeg libsndfile wget libsndfile
+module load Python FFmpeg libsndfile wget binutils
 ##zlib1g liblzma-dev Libsox-fmt-mp3 Libboost-system-dev Libboost-thread-dev Libboost-program-options$
+
 ##--------------------Install python libraries-------------------------------------
-pip install libsndfile1-dev
-pip install libsndfile1
+#pip install libsndfile1-dev
+#pip install libsndfile1
 pip install SoundFile
 #pip install editdistance==0.1
 pip install editdistance 
@@ -13,18 +14,26 @@ pip install tensorboardX
 pip install tqdm
 pip install jiwer
 pip install hydra-ax-sweeper==1.2
+
 ##--------------------Install kenlm-------------------------------------------------
-git clone --quiet https://github.com/kpu/kenlm && cd kenlm && mkdir -p build && cd build && cmake ..$
+cd ~
+git clone --quiet https://github.com/kpu/kenlm && cd kenlm && mkdir -p build && cd build && cmake .. && make -j 4
 pip install https://github.com/kpu/kenlm/archive/master.zip
+
 ##--------------------Install OpenBLAS---------------------------------------------
-git clone --quiet https://github.com/xianyi/OpenBLAS.git && cd OpenBLAS && mkdir -p build && cd buil$
-    cmake .. && make -j 4 && make install
+cd ~
+git clone --quiet https://github.com/xianyi/OpenBLAS.git && cd OpenBLAS && mkdir -p build && cd build && cmake .. && make -j 4 && make install
+
 ##--------------------Install fftw-------------------------------------------------
+cd ~
 wget http://www.fftw.org/fftw-3.3.10.tar.gz && tar -xvf fftw-3.3.10.tar.gz && cd fftw-3.3.10 && \
     mkdir build && cd build && cmake .. && make -j 4 && make install -q
-cd ..
-##--------------------Install Faairseq--------------------------------------------
-git clone https://github.com/pytorch/fairseq &&
-cp configuration_SLURM/setup.py fairseq/. && 
-cp configuration_SLURM/audio_pretraining.py fairseq/fairseq/tasks/. && 
-cp configuration_SLURM/infer.py fairseq/examples/speech_recognition/. 
+
+
+##--------------------Install Fairseq--------------------------------------------
+cd ~
+cd americasNLP
+git clone https://github.com/pytorch/fairseq && \
+cp configuration_SLURM/setup.py fairseq/. && \
+cp configuration_SLURM/audio_pretraining.py fairseq/fairseq/tasks/. && \
+cp configuration_SLURM/infer.py fairseq/examples/speech_recognition/. \
