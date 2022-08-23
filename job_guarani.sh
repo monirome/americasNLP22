3c770e1d1c043e1625dde1872c0cbd1baf5c5226
Monica Romero <74182452+monirome@users.noreply.github.com>
2022-08-23 14:34:49 +0200

    Update job_guarani.sh

--- job_guarani.sh
+++ job_guarani.sh
@@ -3,14 +3,13 @@
 #SBATCH --partition=standard
 #SBATCH --job-name=prueba_job_monica.job
 #SBATCH --ntasks=1
-#SBATCH --mem-per-cpu=50gb
+#SBATCH --mem-per-cpu=100gb
 #SBATCH --partition=standard-gpu
-#SBATCH --gres=gpu:v100
+#SBATCH --gres=gpu:a100 
 ##SBATCH --mail-type=ALL
-#SBATCH --mail-user=romeroferronmonica@gmail.com
+##SBATCH --mail-user=XXX@mail.com
 ##SBATCH --chdir=
 ##------------------------ End job description ------------------------
-module load FFmpeg libsndfile Python 
-module load binutils
+module load Python PyTorch FFmpeg libsndfile wget binutils CMake OpenBLAS FFTW Eigen Boost
 
 srun fairseq-hydra-train --config-dir config --config-name finetune_guarani.yaml 
